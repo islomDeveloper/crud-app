29e7a63ca2366109d1eb16def83ab20521b47a49
islom.dev <islomuzdeveloper@gmail.com>
2021-10-16 23:45:28 +0500

    16.10.2021 // 23:45 last for today

--- server/src/main/java/dev/islom/crudapp/payload/ApiResponse.java
+++ server/src/main/java/dev/islom/crudapp/payload/ApiResponse.java
@@ -1,6 +1,6 @@
 package dev.islom.crudapp.payload;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse {
     private String message;
     private boolean success;
